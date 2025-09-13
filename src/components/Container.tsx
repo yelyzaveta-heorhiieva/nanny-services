673f05313b8a009d7194400d742515b83e441102
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-09-13 17:23:53 +0300

    add styles for container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -9,7 +9,7 @@ export default function Container({ children }: ContainerProps) {
   const location = useLocation();
   return (
     <div
-      className={`xls:px-24 px-8 ${
+      className={`xls:px-24 px-8  mx-auto ${
         location.pathname === '/'
           ? 'xls:max-w-[1376px] xls:min-w-[1376px] min-w-[288px]'
           : 'xls:max-w-[1440px] xls:min-w-[1440px]'
