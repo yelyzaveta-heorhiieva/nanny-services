83ad61719285c723af9585fc17f66ce40802ad9a
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-09-22 18:54:01 +0300

    add styles for navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,17 +1,20 @@
 import { useSelector } from 'react-redux';
 import NavItem from './NavItem';
 import { selectIsLogged } from '../redux/selectors';
+import { useLocation } from 'react-router-dom';
 
 export interface NavigationProps {
   onClick?: () => void;
 }
 
-
-export default function Navigation({onClick}: NavigationProps) {
+export default function Navigation({ onClick }: NavigationProps) {
   const isLogged = useSelector(selectIsLogged);
+  const location = useLocation();
 
   return (
-    <nav className='xl:mr-[217px]'>
+    <nav
+      className={location.pathname === '/' ? 'xl:mr-[92px]' : 'xl:mr-[217px]'}
+    >
       <ul className='flex xl:flex-row flex-col text-xl font-normal text-center xl:text-base leading-[125%] tracking-[-0.01em] gap-10 xl:py-[14px]'>
         <NavItem link='/' name='Home' onClick={onClick} />
         <NavItem link='/nannies' name='Nannies' onClick={onClick} />
